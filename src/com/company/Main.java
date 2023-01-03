88c63c9d32afb7064d13168824575152e29a50ca
Ilmir <sunugatoffi@yandex.ru>
2023-01-03 18:56:40 +0300

    Model OSI

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server started");
+        int port = 8089;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                   System.out.printf("New connect accepted %d%n", clientSocket.getPort());
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s your port is %d", name, clientSocket.getPort()));
+                }
+            }
+        }
+    }
+}
